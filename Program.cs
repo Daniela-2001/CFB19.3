6d5901f1d4a1a190da8d448c3df0ec996bcd8c06
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-25 08:49:42 +0000

    CFB19.3

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace aula19._3
+{
+    internal class Program
+    {
+        static void Main(){
+
+            int[] num = new int[10];
+
+            for(int i = 0; i < 10; i++){
+                num[i] = 0;
+
+                Console.WriteLine(num[i]);
+            }
+        }
+    }
+}
